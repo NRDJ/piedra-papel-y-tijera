08c826940562f8a2c93d4b9b026f65b9b7fb9c08
Nicolas Riedel <nriedeldj@gmail.com>
2020-08-19 15:56:11 -0400

    Added computer play output and 'else'

--- juego.rb
+++ juego.rb
@@ -4,15 +4,17 @@ comp = rand(0..2)
 
 case num
 when "papel"
-    puts "Ganaste" if comp == 0
-    puts "Empataste" if comp == 1
-    puts "Perdiste" if comp == 2
+    puts "Computador juega piedra\nGanaste" if comp == 0
+    puts "Computador juega papel\nEmpataste" if comp == 1
+    puts "Computador juega tijera\nPerdiste" if comp == 2
 when "piedra"
-    puts "Empataste" if comp == 0
-    puts "Perdiste" if comp == 1
-    puts "Ganaste" if comp == 2
+    puts "Computador juega piedra\nEmpataste" if comp == 0
+    puts "Computador juega papel\nPerdiste" if comp == 1
+    puts "Computador juega tijera\nGanaste" if comp == 2
 when "tijera"
-    puts "Perdiste" if comp == 0
-    puts "Ganaste" if comp == 1
-    puts "Empataste" if comp == 2
+    puts "Computador juega piedra\nPerdiste" if comp == 0
+    puts "Computador juega papel\nGanaste" if comp == 1
+    puts "Computador juega tijera\nEmpataste" if comp == 2
+else
+    puts "Argumento inválido: Debe ser piedra, papel o tijera."
 end
